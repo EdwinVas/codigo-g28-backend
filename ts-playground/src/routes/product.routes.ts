8fa30bd11e4cf3584af040be8faa8bb2d97917fe
linder3hs <linderhassinger00@gmail.com>
2026-03-06 21:29:16 -0500

    ts

--- ts-playground/src/routes/product.routes.ts
+++ ts-playground/src/routes/product.routes.ts
@@ -5,5 +5,9 @@ const router = Router();
 const controller = new ProductController();
 
 router.get("/", (req, res) => controller.getAll(req, res));
+router.get("/:id", (req, res) => controller.getById(req, res));
+router.post("/", (req, res) => controller.create(req, res));
+router.put("/:id", (req, res) => controller.update(req, res));
+router.delete("/:id", (req, res) => controller.destroy(req, res));
 
 export default router;
